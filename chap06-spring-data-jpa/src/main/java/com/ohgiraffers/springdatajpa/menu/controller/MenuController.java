52581ac5ec405cd3aa6e277724c9e23a113a9b5f
HAHYEONLEE <adr1025@naver.com>
2023-10-27 16:43:09 +0900

    2023-10-27

--- chap06-spring-data-jpa/src/main/java/com/ohgiraffers/springdatajpa/menu/controller/MenuController.java
+++ chap06-spring-data-jpa/src/main/java/com/ohgiraffers/springdatajpa/menu/controller/MenuController.java
@@ -1,14 +1,22 @@
 package com.ohgiraffers.springdatajpa.menu.controller;
 
+import com.ohgiraffers.springdatajpa.common.Pagenation;
+import com.ohgiraffers.springdatajpa.common.PagingButtonInfo;
+import com.ohgiraffers.springdatajpa.menu.dto.CategoryDTO;
 import com.ohgiraffers.springdatajpa.menu.dto.MenuDTO;
 import com.ohgiraffers.springdatajpa.menu.service.MenuService;
-import org.modelmapper.ModelMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+
+@Slf4j
 @Controller
 @RequestMapping("/menu")
 public class MenuController {
@@ -28,4 +36,128 @@ public class MenuController {
         return "menu/detail";
     }
 
+    /* 페이징 이전 버전 */
+//    @GetMapping("/list")
+//    public String findMenuList(Model model) {
+//        List<MenuDTO> menuList = menuService.findMenuList();
+//        model.addAttribute("menuList", menuList);
+//        return "menu/list";
+//    }
+
+    /* 페이징 이후 버전 */
+    @GetMapping("/list")
+    public String findMenuList (@PageableDefault Pageable pageable, Model model) {
+
+        /* page -> number, size, sort 파라미터가 Pageable 객체에 담긴다. */
+        log.info("pageable : {}", pageable);
+
+        Page<MenuDTO> menuList = menuService.findMenuList(pageable);
+
+        /* Page 객체가 담고 있는 정보 */
+        log.info("조회한 내용 목록 ㅣ {}", menuList.getContent());
+        log.info("총 페이지 수  : {}", menuList.getTotalPages());
+        log.info("총 메뉴 수 : {}", menuList.getTotalElements());
+        log.info("해당 페이지에 표시 될 요소 수 : {}", menuList.getSize());
+        log.info("해당 페이지에 실제 요소 수 : {}", menuList.getNumberOfElements());
+        log.info("첫 페이지 여부 : {}", menuList.isFirst());
+        log.info("마지막 페이지 여부 : {}", menuList.isLast());
+        log.info("정렬 방식 : {}", menuList.getSort());
+        log.info("여러 페이지 중 현재 인덱스 : {}", menuList.getNumber());
+
+        PagingButtonInfo paging = Pagenation.getPagingButtonInfo(menuList);
+
+        model.addAttribute("menuList", menuList);
+        model.addAttribute("paging", paging);
+
+        return "menu/list";
+    }
+
+    @GetMapping("/querymethod")
+    public void queryMethodPage(){}
+
+    @GetMapping("/search")
+    public String findByMenuPrice(@RequestParam Integer menuPrice, Model model){
+
+        List<MenuDTO> menuList = menuService.findByMenuPrice(menuPrice);
+
+
+        model.addAttribute("menuList", menuList);
+
+        return "menu/searchResult";
+    }
+
+    @GetMapping("/searchName")
+    public String findByMenuName(@RequestParam String menuName, Model model) {
+
+        List<MenuDTO> menuList = menuService.findByMenuName(menuName);
+        model.addAttribute("menuList", menuList);
+
+        return "menu/searchName";
+    }
+
+    @GetMapping("/searchCategory")
+    public String findByCategoryCode(@RequestParam Integer categoryCode, Model model) {
+
+        List<MenuDTO> menuList = menuService.findByCategoryCode(categoryCode);
+        model.addAttribute("menuList", menuList);
+
+        return "menu/searchCategory";
+    }
+
+    @GetMapping("/categoryList")
+    @ResponseBody
+    public List<CategoryDTO> findCategoryList1(){
+        return menuService.findAllCategory();
+    }
+
+    @GetMapping("/searchNameAndPrice")
+    public String findByNameAndPrice(@RequestParam Integer menuPrice,
+                                     @RequestParam String menuName,
+                                     Model model) {
+
+        List<MenuDTO> menuList = menuService.findByNameAndPrice(menuPrice, menuName);
+        model.addAttribute("menuList", menuList);
+
+        return "menu/searchNameAndPrice";
+    }
+
+
+    @GetMapping("/regist")
+    public void registPage() {}
+
+    @GetMapping("/category")
+    @ResponseBody
+    public List<CategoryDTO> findCategoryList(){
+        return menuService.findAllCategory();
+    }
+
+    @PostMapping("/regist")
+    public String registMenu(MenuDTO menu) {
+
+        menuService.registNewMenu(menu);
+
+        return "redirect:/menu/list";
+    }
+
+    @GetMapping("/modify")
+    public void modifyPage(){}
+
+    @PostMapping("/modify")
+    public String modifyMenu(MenuDTO menu) {
+
+        menuService.modifyMenu(menu);
+
+        return "redirect:/menu/" + menu.getMenuCode();
+    }
+
+    @GetMapping("/delete")
+    public void deletePage() {}
+
+    @PostMapping("/delete")
+    public String deleteMenu(@RequestParam Integer menuCode) {
+
+        menuService.deleteMenu(menuCode);
+
+        return "redirect:/menu/list";
+    }
 }
--- chap06-spring-data-jpa/src/main/java/com/ohgiraffers/springdatajpa/menu/service/MenuService.java
+++ chap06-spring-data-jpa/src/main/java/com/ohgiraffers/springdatajpa/menu/service/MenuService.java
@@ -1,22 +1,36 @@
 package com.ohgiraffers.springdatajpa.menu.service;
 
+import com.ohgiraffers.springdatajpa.menu.dto.CategoryDTO;
 import com.ohgiraffers.springdatajpa.menu.dto.MenuDTO;
+import com.ohgiraffers.springdatajpa.menu.entity.Category;
 import com.ohgiraffers.springdatajpa.menu.entity.Menu;
+import com.ohgiraffers.springdatajpa.menu.repository.CategoryRepository;
 import com.ohgiraffers.springdatajpa.menu.repository.MenuRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MenuService {
 
     private final MenuRepository menuRepository;
+    private final CategoryRepository categoryRepository;
     private final ModelMapper modelMapper;
 
-    public MenuService(MenuRepository menuRepository,  ModelMapper modelMapper) {
+
+    public MenuService(MenuRepository menuRepository, CategoryRepository categoryRepository,  ModelMapper modelMapper) {
         this.menuRepository = menuRepository;
+        this.categoryRepository = categoryRepository;
         this.modelMapper = modelMapper;
     }
-
+    /* 1. id로 entity 조회 : findById */
     public MenuDTO findMenuByCode(int menuCode) {
 
         // Entity로 조회 한 뒤 비영속 객체인 MenuDTO로 변환해서 반환한다.
@@ -24,4 +38,87 @@ public class MenuService {
 
         return modelMapper.map(menu, MenuDTO.class);
     }
+
+    /* 2-1. 모든 entity 조회 : findAll */
+    public List<MenuDTO> findMenuList() {
+
+        List<Menu> menuList =  menuRepository.findAll(Sort.by("menuCode").descending()); // 필드명을 작성해야함
+
+        return menuList.stream().map(menu -> modelMapper.map(menu, MenuDTO.class)).collect(Collectors.toList());
+    }  //Stream<Menu>로 바뀐부분 //stream이 가지고 있는 map이라는 메소드로 가공 중 Stream<MenuDTO> // 얘를 다시 collection 타입으로 그 중에서도 ㅣist 타입으로 바꿔줘라~
+
+    /* 2-2. 페이징 된 entity 죄회 : findAll(Pageable) */
+    public Page<MenuDTO> findMenuList(Pageable pageable) {
+
+        pageable = PageRequest.of(pageable.getPageNumber() <=0 ? 0 : pageable.getPageNumber() -1,
+                pageable.getPageSize(),
+                Sort.by("MenuCode").descending());
+
+        Page<Menu> menuList = menuRepository.findAll(pageable);
+
+        return menuList.map(menu -> modelMapper.map(menu, MenuDTO.class));
+    }
+
+    /* 3. Query Method Test */
+    public List<MenuDTO> findByMenuPrice(Integer menuPrice) {
+
+        //List<Menu> menuList = menuRepository.findByMenuPriceGreaterThan(menuPrice);
+        //List<Menu> menuList = menuRepository.findByMenuPriceGreaterThanOrderByMenuPrice(menuPrice);
+        List<Menu> menuList = menuRepository.findByMenuPriceGreaterThan(menuPrice, Sort.by("menuPrice").descending());
+
+        return menuList.stream().map(menu -> modelMapper.map(menu, MenuDTO.class)).collect(Collectors.toList());
+    }
+
+    /* 4. JPQL or Native Query Test */
+    public List<CategoryDTO> findAllCategory() {
+
+        List<Category> categoryList = categoryRepository.findAllCategory();
+
+        return categoryList.stream()
+                .map(category -> modelMapper.map(category, CategoryDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    /* 5. Entity 저장 */
+    @Transactional
+    public void registNewMenu(MenuDTO menu) {
+
+        menuRepository.save(modelMapper.map(menu, Menu.class));
+    }
+
+    /* 6. Entity 수정 */
+    @Transactional
+    public void modifyMenu(MenuDTO menu) {
+        Menu founMenu = menuRepository.findById(menu.getMenuCode()).orElseThrow(IllegalArgumentException::new);
+        founMenu.setMenuName(menu.getMenuName());
+    }
+
+    /* 7. Entity 삭제 */
+    @Transactional
+    public void deleteMenu(Integer menuCode) {
+        menuRepository.deleteById(menuCode);
+    }
+
+    public List<MenuDTO> findByMenuName(String menuName) {
+
+        List<Menu> menuList = menuRepository.findByMenuNameContaining(menuName);
+
+        return menuList.stream().map(menu -> modelMapper.map(menu, MenuDTO.class)).collect(Collectors.toList());
+    }
+
+
+    public List<MenuDTO> findByCategoryCode(Integer categoryCode) {
+
+        List<Menu> menuList = menuRepository.findByCategoryCode(categoryCode);
+
+        return menuList.stream().map(menu -> modelMapper.map(menu, MenuDTO.class)).collect(Collectors.toList());
+    }
+
+    public List<MenuDTO> findByNameAndPrice(Integer menuPrice, String menuName) {
+
+        List<Menu> menuList = menuRepository.findByMenuPriceLessThanAndMenuNameContaining(menuPrice, menuName);
+        return menuList.stream().map(menu -> modelMapper.map(menu, MenuDTO.class)).collect(Collectors.toList());
+    }
+
+
 }
